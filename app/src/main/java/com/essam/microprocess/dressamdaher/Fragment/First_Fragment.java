380df0c42660a0558383734a95e1b61d698c1cb3
alaadah95 <alaadah_331@yahoo.com>
2018-09-29 13:57:18 +0200

    add first fragment

---
+++ app/src/main/java/com/essam/microprocess/dressamdaher/Fragment/First_Fragment.java
@@ -0,0 +1,57 @@
+package com.essam.microprocess.dressamdaher.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.essam.microprocess.dressamdaher.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class First_Fragment extends Fragment {
+
+@BindView(R.id.Goto_signin)
+    Button Sign_in;
+
+@BindView(R.id.Goto_signup)
+Button Sign_up;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         View v =inflater.inflate(R.layout.fragment_first_, container, false);
+         ButterKnife.bind(this,v); // intialize butterknife .
+
+
+
+         Sign_in.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 // add fragment (sign in )
+
+             }
+         });
+
+         Sign_up.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 // add fragment (sign up )
+
+             }
+         });
+
+
+
+        return v;
+    }
+
+}
--- app/src/main/java/com/essam/microprocess/dressamdaher/Views/MainActivity.java
+++ app/src/main/java/com/essam/microprocess/dressamdaher/Views/MainActivity.java
@@ -3,6 +3,7 @@ package com.essam.microprocess.dressamdaher.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.essam.microprocess.dressamdaher.Fragment.First_Fragment;
 import com.essam.microprocess.dressamdaher.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().
+                beginTransaction().
+                replace(R.id.Main_fragment,new First_Fragment())
+                .addToBackStack(null).commit();
+
     }
 }
