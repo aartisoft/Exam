c806d1ddf42ea2b320785fa50e482f0af38650bc
medoo0 <raslanmohamed156@yahoo.com>
2018-09-29 03:49:58 -0700

    Orgaize Project

--- app/src/main/java/com/essam/microprocess/dressamdaher/Views/MainActivity.java
+++ app/src/main/java/com/essam/microprocess/dressamdaher/Views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.essam.microprocess.dressamdaher;
+package com.essam.microprocess.dressamdaher.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.essam.microprocess.dressamdaher.R;
 
 public class MainActivity extends AppCompatActivity {
 
