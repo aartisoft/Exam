45da4e3d6e3bf5526abf06e95124e1f2abd81fcd
alaadah95 <alaadah_331@yahoo.com>
2018-09-30 23:43:07 +0200

    finishing Login and signup 2 .

--- app/src/main/java/com/essam/microprocess/dressamdaher/Views/ControlPanel.java
+++ app/src/main/java/com/essam/microprocess/dressamdaher/Views/ControlPanel.java
@@ -10,6 +10,6 @@ public class ControlPanel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exam);
+        setContentView(R.layout.activity_controlpanel);
     }
 }
