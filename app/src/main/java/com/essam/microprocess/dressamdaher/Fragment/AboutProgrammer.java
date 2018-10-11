0c7b680e2829fcc1454de6f143f5d253e71c25c6
alaadah95 <alaadah_331@yahoo.com>
2018-10-11 18:55:48 +0200

    about programmer

--- app/src/main/java/com/essam/microprocess/dressamdaher/Fragment/AboutProgrammer.java
+++ app/src/main/java/com/essam/microprocess/dressamdaher/Fragment/AboutProgrammer.java
@@ -2,6 +2,7 @@ package com.essam.microprocess.dressamdaher.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -39,6 +40,13 @@ public class AboutProgrammer extends Fragment {
     ScrollView Scrollview;
 
     int MohamedX ;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
